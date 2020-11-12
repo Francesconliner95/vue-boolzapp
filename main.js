e36850123ca097b90acec50c8aefbb63facc89e5
Francesconliner <francescodivincenzo95@outlook.it>
2020-11-12 15:56:52 +0100

    Milestone 2

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#root',
     data: {
+        c_index: 0,
         img: [
             "img/utente_1.png",
             "img/utente_2.png",
@@ -93,22 +94,9 @@ var app = new Vue({
                     ]
         },
     methods: {
-        // add_item: function(){
-        //     this.array_items.push(this.item);
-        //     console.log(this.array_items);
-        // },
-        // remove_item: function(item){
-        //     console.log(item);
-        //     /*con la funzione splice abbiamo la possibilità di modificare o sostituire il nostro array, il primo valore corrisponealla posizione sulla quale vogliamo andare ad agire, il secondo valore se settato ad 0 inserisce un elelmento in quella posizione facendo scalare tutti gli altri, se settato ad 1 sostituisce l'elemento con un dato impostato da noi nel terzo valore*/
-        //     this.array_items.splice(item, 1);
-        //     console.log(this.array_items);
-        // },
-        // /*creiamo la funzione enter, dve ci passiamo il parametro "keyinput dall'html viene confrontato con il numero 13(valore di invio nella tastiera), se si viene chimata la funzione aggiungi item "*/
-        // enter: function(key_input) {
-        //     if (key_input.keyCode === 13) {
-        //     console.log('entrato');
-        //     this.add_item();
-        //     }
-        // },
+        set_contact(indice){
+            console.log(indice);
+            this.c_index = indice;
+        }
     },
 })
