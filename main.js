a0fcca151a8e84c6eec654fd366a521c6338976e
Francesconliner <francescodivincenzo95@outlook.it>
2020-11-12 14:52:03 +0100

    Milestone 1

--- main.js
+++ main.js
@@ -1,8 +1,96 @@
 var app = new Vue({
     el: '#root',
     data: {
-        // item: 'Ciao!',
-        // array_items: [],
+        img: [
+            "img/utente_1.png",
+            "img/utente_2.png",
+            "img/utente_3.png",
+            "img/utente_4.png",
+        ],
+        contacts:   [
+                        {
+                        name: 'Michele',
+                        avatar: '_1',
+                        visible: true,
+                        messages: [
+                                    {
+                                    date: '10/01/2020 15:30:55',
+                                    message: 'Hai portato a spasso il cane?',
+                                    status: 'sent'
+                                    },
+                                    {
+                                    date: '10/01/2020 15:50:00',
+                                    message: 'Ricordati di dargli da mangiare',
+                                    status: 'sent'
+                                    },
+                                    {
+                                    date: '10/01/2020 16:15:22',
+                                    message: 'Tutto fatto!',
+                                    status: 'received'
+                                    }
+                                ],
+                        },
+                        {
+                        name: 'Fabio',
+                        avatar: '_2',
+                        visible: true,
+                        messages: [
+                                    {
+                                    date: '20/03/2020 16:30:00',
+                                    message: 'Ciao come stai?',
+                                    status: 'sent'
+                                    },
+                                    {
+                                    date: '20/03/2020 16:30:55',
+                                    message: 'Bene grazie! Stasera ci vediamo?',
+                                    status: 'received'
+                                    },
+                                    {
+                                    date: '20/03/2020 16:35:00',
+                                    message: 'Mi piacerebbe ma devo andare a fare la spesa',
+                                    status: 'received'
+                                    }
+                                ],
+                        },
+                        {
+                        name: 'Batman',
+                        avatar: '_3',
+                        visible: true,
+                        messages: [
+                                    {date: '28/03/2020 10:10:40',
+                                    message: 'Sono l\'eroe che Gotham merita, ma non quello di cui ha bisogno adesso',
+                                    status: 'received'
+                                    },
+                                    {
+                                    date: '28/03/2020 10:20:10',
+                                    message: 'Sicuro di non aver sbagliato chat?',
+                                    status: 'sent'
+                                    },
+                                    {
+                                    date: '28/03/2020 16:15:22',
+                                    message: 'Ah scusa!',
+                                    status: 'received'
+                                    }
+                                ],
+                        },
+                        {
+                        name: 'Luisa',
+                        avatar: '_4',
+                        visible: true,
+                        messages: [
+                                    {
+                                    date: '10/01/2020 15:30:55',
+                                    message: 'Lo sai che ha aperto una nuova pizzeria?',
+                                    status: 'sent'
+                                    },
+                                    {
+                                    date: '10/01/2020 15:50:00',
+                                    message: 'Si, ma preferirei andare al cinema',
+                                    status: 'received'
+                                    }
+                                ],
+                        },
+                    ]
         },
     methods: {
         // add_item: function(){
