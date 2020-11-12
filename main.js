f4352f94b9da2bcc0459022745c23c25b23bbd2f
Francesconliner <francescodivincenzo95@outlook.it>
2020-11-12 13:22:56 +0100

    Interfaccia

---
+++ main.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        // item: 'Ciao!',
+        // array_items: [],
+        },
+    methods: {
+        // add_item: function(){
+        //     this.array_items.push(this.item);
+        //     console.log(this.array_items);
+        // },
+        // remove_item: function(item){
+        //     console.log(item);
+        //     /*con la funzione splice abbiamo la possibilità di modificare o sostituire il nostro array, il primo valore corrisponealla posizione sulla quale vogliamo andare ad agire, il secondo valore se settato ad 0 inserisce un elelmento in quella posizione facendo scalare tutti gli altri, se settato ad 1 sostituisce l'elemento con un dato impostato da noi nel terzo valore*/
+        //     this.array_items.splice(item, 1);
+        //     console.log(this.array_items);
+        // },
+        // /*creiamo la funzione enter, dve ci passiamo il parametro "keyinput dall'html viene confrontato con il numero 13(valore di invio nella tastiera), se si viene chimata la funzione aggiungi item "*/
+        // enter: function(key_input) {
+        //     if (key_input.keyCode === 13) {
+        //     console.log('entrato');
+        //     this.add_item();
+        //     }
+        // },
+    },
+})
